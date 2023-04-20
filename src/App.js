2edf2789b6eee50578a41db30a3d1b5411cd2b76
louaybouzabia <louaybouzabia@gmail.com>
2023-04-20 20:05:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction, RemoveTodoAction } from './Actions';
 
 function App() {
+
+  const [todo, setTodo] = useState();
+
+  const dispatch = useDispatch();
+  const Todo = useSelector(state => state.Todo);
+  const { todos } = Todo;
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoAction(todo))
+  };
+  const removeHandler = (t) => {
+    dispatch(RemoveTodoAction(t));
+  }
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App_header'>
+        <h2>Checkpoint Redux</h2>
+        <form onSubmit={handelSubmit}>
+          <input placeholder='you can enter a Todo'
+            onChange={(e) => setTodo(e.target.value)}></input>
+          <button type='submit'>Add</button>
+        </form>
+        <ul className='allTodos'>
+          {
+            todos && todos.map((t) => (
+              <li key={t.id} className='singleTodo'>
+                <span className='todoText'>{t.todo}</span>
+                <button onClick={() => removeHandler(t)}>Delete</button>
+              </li>
+            ))
+          }
+
+        </ul>
       </header>
     </div>
   );
